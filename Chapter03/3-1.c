4c475c141a1de95a84b1959e1220f2fe36bf8046
SpecificGenerality <justiny@princeton.edu>
2018-06-16 13:51:06 -0400

    so much work

--- Chapter03/3-1.c
+++ Chapter03/3-1.c
@@ -1,2 +1,49 @@
-#include "linkedlist.c"
+#include "stack.h"
+#include "reader.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+enum Boolean { FALSE, TRUE };
+
+static int isValid(const char * pcLine) {
+   char cChar;
+   cChar = *pcLine;
+   while (cChar != '\0') {
+      if (cChar != '(' || cChar != ')') return FALSE;
+   }
+   return TRUE;
+}
+int isValidNesting(const char * pcLine) {
+   Stack_T psStack;
+   char cChar;
+   void * pvPop;
+   char * pcLineCpy;
+   assert(pcLine != NULL);
+   assert(isValid(pcLine));
+
+   psStack = Stack_new();
+   pcLineCpy = (char *) pcLine;
+   pvPop = NULL;
+
+   while((cChar = *pcLineCpy++) != '\0') {
+      if (cChar == '(') Stack_push(psStack, &cChar);
+      else pvPop = Stack_pop(psStack);
+      if (pvPop == NULL) {
+         Stack_free(psStack);
+         return FALSE;
+      }
+   }
+   return TRUE;
+}
+
+int main(int argc, char *argv[]) {
+   char * pcLine;
+
+   while ((pcLine = readLine(stdin)) != NULL)  {
+      if (isValidNesting(pcLine)) fprintf(stdout, "%s\n", "valid");
+      else fprintf(stdout, "%s\n", "invalid");
+      free(pcLine);
+   }
+}
---
+++ Chapter03/reader.c
@@ -0,0 +1,67 @@
+char * readLine(FILE *psFile)
+{
+   /* line length starts at size two characters */
+   const size_t INITIAL_LINE_LENGTH = 2;
+   /* line length grows by this factor upon resizing*/
+   const size_t GROWTH_FACTOR = 2;
+
+   /* number of characters in the string */
+   size_t uLineLength = 0;
+   /* number of characters that the string can store */
+   size_t uPhysLineLength = INITIAL_LINE_LENGTH;
+   /* the string storing characters from psFile */
+   char *pcLine;
+   /* character read in from psFile*/
+   int iChar;
+
+   assert(psFile != NULL);
+
+   /* If no lines remain, return NULL. */
+   if (feof(psFile))
+      return NULL;
+   iChar = fgetc(psFile);
+   if (iChar == EOF)
+      return NULL;
+
+   /* Allocate memory for the string. */
+   pcLine = (char*)malloc(uPhysLineLength);
+   if (pcLine == NULL)
+      {
+         fprintf(stderr,"%s %s\n", pcPgmName, 
+            "readLine: memory allocation failed");
+         exit(EXIT_FAILURE);
+      }
+
+   /* Read characters into the string. */
+   while ((iChar != '\n') && (iChar != EOF))
+   {
+      if (uLineLength == uPhysLineLength)
+      {
+         uPhysLineLength *= GROWTH_FACTOR;
+         pcLine = (char*)realloc(pcLine, uPhysLineLength);
+         if (pcLine == NULL)
+            {
+               fprintf(stderr,"%s %s\n", pcPgmName, 
+                  "readLine: memory allocation failed");
+               exit(EXIT_FAILURE);}
+      }
+      pcLine[uLineLength] = (char)iChar;
+      uLineLength++;
+      iChar = fgetc(psFile);
+   }
+
+   /* Append a null character to the string. */
+   if (uLineLength == uPhysLineLength)
+   {
+      uPhysLineLength++;
+      pcLine = (char*)realloc(pcLine, uPhysLineLength);
+      if (pcLine == NULL)
+         {
+            fprintf(stderr,"%s %s\n", pcPgmName, 
+               "readLine: memory allocation failed");
+            exit(EXIT_FAILURE);}
+   }
+   pcLine[uLineLength] = '\0';
+
+   return pcLine;
+}
