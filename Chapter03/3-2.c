844ddfb0821896fd630983e0475497f7efedc28f
SpecificGenerality <justiny@princeton.edu>
2018-06-17 13:28:29 -0400

    finished 3-2: untested

--- Chapter03/3-2.c
+++ Chapter03/3-2.c
@@ -1,10 +1,40 @@
 #include "linkedlist.h"
+#include "reader.h"
 #include <assert.h>
+#include <stdio.h>
 
-LList_T LList_reverse(LList_T psLList) {
+void LList_reverse(LList_T psLList) {
    Node_T psCurrent;
    Node_T psNext;
-   
+   Node_T psPrev;
+
    assert(psLList != NULL);
 
+   psCurrent = psLList -> psFirst;
+   psNext = psCurrent -> psNext;
+   psPrev = NULL;
+
+   while (psNext != NULL) {
+      psCurrent -> psNext = psPrev;
+      psPrev = psCurrent;
+      psCurrent = psNext;
+      psNext = psNext -> psNext; 
+   }
+
+   psLList -> psFirst = psCurrent;
+}
+
+int main(void) {
+   char * pcLine;
+   LList_T psLList;
+
+   while ((pcLine = readLine(stdin)) != NULL) {
+      psLList = LList_create(pcLine);
+      LList_print(psLList);
+      LList_reverse(psLList);
+      LList_print(psLList);
+      LList_free(psLList)
+      free(pcLine)
+   }
+   return 0;
 }
